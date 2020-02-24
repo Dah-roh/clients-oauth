59ae42ae3248410f9312e40582241bcb72a14f75
dizsart <darogadibia@gmail.com>
2020-02-24 13:38:06 +0100

    add some file and class changes

---
+++ src/test/java/com/server/clients/ClientsApplicationTests.java
@@ -0,0 +1,14 @@
+package com.server.clients;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
